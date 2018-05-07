773c6d49cbaabb0fce445a25ddaae15ff9893c86
Darren Ethier <darren@roughsmootheng.in>
2018-05-06 21:04:53 -0400

    fix phpcs violations (#3)

--- EED_Affiliate_WP.module.php
+++ EED_Affiliate_WP.module.php
@@ -1,247 +1,243 @@
 <?php
-/**
- * This file contains the module for the EE Affiliate addon
- *
- * @since 1.0.0
- * @package  EE Affiliate Addon
- * @subpackage modules
- */
-if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+
 /**
  *
  * EE Affiliate module.  Regsiters hooks and callbacks for processing affiliate conversion tracking.
  *
  * @since 1.0.0
  *
- * @package		EE Affiliate Addon
- * @subpackage	modules
- * @author 		Darren Ethier
+ * @package     EE Affiliate Addon
+ * @subpackage  modules
+ * @author      Darren Ethier
  *
  * ------------------------------------------------------------------------
  */
-class EED_Affiliate_WP extends EED_Module {
+class EED_Affiliate_WP extends EED_Module
+{
 
 
-	/**
-	 * This is used for the affwp context resource reference for each affiliate record
-	 * in the affwp tables.
-	 *
-	 * @type string
-	 */
-	protected static $_context = 'event_espresso';
+    /**
+     * This is used for the affwp context resource reference for each affiliate record
+     * in the affwp tables.
+     *
+     * @type string
+     */
+    protected static $_context = 'event_espresso';
 
 
-	public static function instance() {
-		return parent::get_instance( __CLASS__ );
-	}
+    public static function instance()
+    {
+        return parent::get_instance(__CLASS__);
+    }
 
-	public static function set_hooks() {
-		add_action( 'AHEE__EE_Payment_Processor__update_txn_based_on_payment', array( 'EED_Affiliate_WP', 'create_referral_record' ), 10, 2 );
-		add_action( 'AHEE__EE_Transaction_Processor__update_transaction_and_registrations_after_checkout_or_payment', array( 'EED_Affiliate_WP', 'update_referral_record' ), 10 );
-		add_action( 'AHEE__EEM_Transaction__delete_junk_transactions__successful_deletion', array( 'EED_Affiliate_WP', 'set_affiliate_referral_status_after_deleted_transaction' ) );
-		add_action( 'AHEE__EE_SPCO_Reg_Step_Finalize_Registration__process_reg_step__completed', array( 'EED_Affiliate_WP', 'maybe_create_referral_record_from_finalize_registration_step' ), 10, 2 );
-	}
+    public static function set_hooks()
+    {
+        add_action('AHEE__EE_Payment_Processor__update_txn_based_on_payment', array( 'EED_Affiliate_WP', 'create_referral_record' ), 10, 2);
+        add_action('AHEE__EE_Transaction_Processor__update_transaction_and_registrations_after_checkout_or_payment', array( 'EED_Affiliate_WP', 'update_referral_record' ), 10);
+        add_action('AHEE__EEM_Transaction__delete_junk_transactions__successful_deletion', array( 'EED_Affiliate_WP', 'set_affiliate_referral_status_after_deleted_transaction' ));
+        add_action('AHEE__EE_SPCO_Reg_Step_Finalize_Registration__process_reg_step__completed', array( 'EED_Affiliate_WP', 'maybe_create_referral_record_from_finalize_registration_step' ), 10, 2);
+    }
 
-	public static function set_hooks_admin() {
-		//covers ajax requests
-		add_action( 'AHEE__EE_Payment_Processor__update_txn_based_on_payment', array( 'EED_Affiliate_WP', 'create_referral_record' ), 10, 2 );
-		add_action( 'AHEE__EE_Transaction_Processor__update_transaction_and_registrations_after_checkout_or_payment', array( 'EED_Affiliate_WP', 'update_referral_record' ), 10 );
-		add_action( 'AHEE__EEM_Transaction__delete_junk_transactions__successful_deletion', array( 'EED_Affiliate_WP', 'set_affiliate_referral_status_after_deleted_transaction' ) );
-		add_action( 'AHEE__EE_SPCO_Reg_Step_Finalize_Registration__process_reg_step__completed', array( 'EED_Affiliate_WP', 'maybe_create_referral_record_from_finalize_registration_step' ), 10, 2 );
-		add_action( 'AHEE__Transactions_Admin_Page__apply_payments_or_refund__after_recording', array( 'EED_Affiliate_WP', 'update_referral_record' ), 10 );
-	}
-
-
-	public function run( $WP ) {
-		//this shouldn't ever be instantiated really which is why we're doing nothing here.
-		return;
-	}
+    public static function set_hooks_admin()
+    {
+        // covers ajax requests
+        add_action('AHEE__EE_Payment_Processor__update_txn_based_on_payment', array( 'EED_Affiliate_WP', 'create_referral_record' ), 10, 2);
+        add_action('AHEE__EE_Transaction_Processor__update_transaction_and_registrations_after_checkout_or_payment', array( 'EED_Affiliate_WP', 'update_referral_record' ), 10);
+        add_action('AHEE__EEM_Transaction__delete_junk_transactions__successful_deletion', array( 'EED_Affiliate_WP', 'set_affiliate_referral_status_after_deleted_transaction' ));
+        add_action('AHEE__EE_SPCO_Reg_Step_Finalize_Registration__process_reg_step__completed', array( 'EED_Affiliate_WP', 'maybe_create_referral_record_from_finalize_registration_step' ), 10, 2);
+        add_action('AHEE__Transactions_Admin_Page__apply_payments_or_refund__after_recording', array( 'EED_Affiliate_WP', 'update_referral_record' ), 10);
+    }
 
 
-
-	/**
-	 * Callback for AHEE__thank_you_page_transaction_details_template__after_transaction_table_row that we'll use to update
-	 * any referrals for Affiliates that have already been created.
-	 *
-	 * @param EE_Transaction|null $transaction
-	 */
-	public static function update_referral_record( $transaction ) {
-		do_action( 'AHEE_log', __FILE__, __FUNCTION__, $transaction->is_completed(), 'transaction is completed check for affiliate wp callback' );
-
-		$awp = function_exists( 'affiliate_wp' ) ? affiliate_wp() : null;
-
-		//first determine if we have a valid affiliate_wp instance or EE_Transaction object and can even work with it!
-		if (
-			! $awp instanceof Affiliate_WP
-			|| ! $transaction instanceof EE_Transaction
-		) {
-			return;
-		}
-
-		//IF we have a referral, update.  Otherwise we do nothing.
-		if ( $referral = $awp->referrals->get_by( 'reference', $transaction->ID(), self::$_context ) ) {
-			self::_maybe_update_affiliate_referral_status( $transaction, $awp, $referral );
-		}
-
-	}
-
-
-	/**
-	 * Callback for AHEE__EE_SPCO_Reg_Step_Finalize_Registration__process_reg_step__completed that is used for creating
-	 * a referral record in the case where a payment isn't being made (example when an Event has the Default Registration
-	 * status set to Not Approved)
-	 *
-	 * @param EE_Checkout $checkout
-	 * @param array       $transaction_update_parameters
-	 */
-	public static function maybe_create_referral_record_from_finalize_registration_step( EE_Checkout $checkout, $transaction_update_parameters ) {
-		//this just wraps our existing create referral method.
-		self::create_referral_record( $checkout->transaction, $checkout->payment );
-	}
-
-
-	/**
-	 * Callback for AHEE__EE_Payment_Processor__update_txn_based_on_payment that is used to create the initial referral record
-	 * if possible.
-	 *
-	 * @param EE_Transaction $transaction
-	 * @param EE_Payment|null     $payment
-	 */
-	public static function create_referral_record( EE_Transaction $transaction, $payment ) {
-		$awp = function_exists( 'affiliate_wp' ) ? affiliate_wp() : null;
-		if (
-			$awp instanceof Affiliate_WP
-			&& ! $awp->referrals->get_by( 'reference', $transaction->ID(), self::$_context ) //don't create if its already present.
-		) {
-			self::_maybe_initiate_affiliate_tracking( $transaction, $awp );
-		}
-	}
+    public function run($WP)
+    {
+        // this shouldn't ever be instantiated really which is why we're doing nothing here.
+        return;
+    }
 
 
 
+    /**
+     * Callback for AHEE__thank_you_page_transaction_details_template__after_transaction_table_row that we'll use to update
+     * any referrals for Affiliates that have already been created.
+     *
+     * @param EE_Transaction|null $transaction
+     */
+    public static function update_referral_record($transaction)
+    {
+        do_action('AHEE_log', __FILE__, __FUNCTION__, $transaction->is_completed(), 'transaction is completed check for affiliate wp callback');
 
-	/**
-	 * Callback for the AHEE__EEM_Transaction__delete_junk_transactions__successful_deletion action hook.
-	 * Used to ensure that if there are any affiliate records matching the deleted transaction id that we delete those
-	 * referrals.
-	 *
-	 * @param $deleted_transaction_ids
-	 */
-	public static function set_affiliate_referral_status_after_deleted_transaction( $deleted_transaction_ids ) {
-		if ( is_array( $deleted_transaction_ids ) ) {
-			$awp = function_exists( 'affiliate_wp' ) ? affiliate_wp() : null;
-			if (
-				$awp instanceof Affiliate_WP
-			) {
-				foreach ( $deleted_transaction_ids as $txn_id ) {
-					if ( $referral = $awp->referrals->get_by( 'reference', $txn_id, self::$_context ) ) {
-						if (
-							is_object( $referral )
-							&& function_exists( 'affwp_delete_referral' )
-							&& $referral->status !== 'paid'
-						) {
-							affwp_delete_referral( $referral );
-						}
-					}
-				}
-			}
-		}
-	}
+        $awp = function_exists('affiliate_wp') ? affiliate_wp() : null;
+
+        // first determine if we have a valid affiliate_wp instance or EE_Transaction object and can even work with it!
+        if (! $awp instanceof Affiliate_WP
+            || ! $transaction instanceof EE_Transaction
+        ) {
+            return;
+        }
+
+        // IF we have a referral, update.  Otherwise we do nothing.
+        if ($referral = $awp->referrals->get_by('reference', $transaction->ID(), self::$_context)) {
+            self::_maybe_update_affiliate_referral_status($transaction, $awp, $referral);
+        }
+    }
 
 
-	/**
-	 * This will update a AffiliateWP referral record if it hasn't been paid according to the transaction status.
-	 *
-	 * @param EE_Transaction $transaction
-	 * @param Affiliate_WP   $awp
-	 * @param                $referral
-	 */
-	protected static function _maybe_update_affiliate_referral_status( EE_Transaction $transaction, Affiliate_WP $awp, $referral ) {
-		//if $referral isn't an object or has already been paid, then get out
-		if (
-			! is_object( $referral )
-			|| $referral->status === 'paid'
-			|| ! function_exists( 'affwp_set_referral_status' )
-		) {
-			return;
-		}
+    /**
+     * Callback for AHEE__EE_SPCO_Reg_Step_Finalize_Registration__process_reg_step__completed that is used for creating
+     * a referral record in the case where a payment isn't being made (example when an Event has the Default Registration
+     * status set to Not Approved)
+     *
+     * @param EE_Checkout $checkout
+     * @param array       $transaction_update_parameters
+     */
+    public static function maybe_create_referral_record_from_finalize_registration_step(EE_Checkout $checkout, $transaction_update_parameters)
+    {
+        // this just wraps our existing create referral method.
+        self::create_referral_record($checkout->transaction, $checkout->payment);
+    }
 
-		//if transaction is complete, then let's update the status to unpaid and also ensure the total is correct for
-		// the current transaction. Otherwise we make sure the status is pending.
-		if ( $transaction->is_completed() ) {
-			//valid affiliate so let's get creating the initial affiliate record.
-			$invoice_amount = $transaction->total() > 0 ? affwp_calc_referral_amount( $transaction->total(), $referral->affiliate_id ) : 0;
-			affwp_set_referral_status( $referral->referral_id, 'unpaid' );
 
-			if ( $invoice_amount !== $referral->amount ) {
-				$awp->referrals->update( $referral->referral_id, array( 'amount' => $invoice_amount ) );
-			}
-		} else {
-			affwp_set_referral_status( $referral->referral_id, 'pending' );
-		}
-	}
+    /**
+     * Callback for AHEE__EE_Payment_Processor__update_txn_based_on_payment that is used to create the initial referral record
+     * if possible.
+     *
+     * @param EE_Transaction $transaction
+     * @param EE_Payment|null     $payment
+     */
+    public static function create_referral_record(EE_Transaction $transaction, $payment)
+    {
+        $awp = function_exists('affiliate_wp') ? affiliate_wp() : null;
+        if ($awp instanceof Affiliate_WP
+            && ! $awp->referrals->get_by('reference', $transaction->ID(), self::$_context) // don't create if its already present.
+        ) {
+            self::_maybe_initiate_affiliate_tracking($transaction, $awp);
+        }
+    }
 
 
 
 
-	/**
-	 * This will create an affiliate wp referral record if there is an affiliate ID in the request.  Note, this does NOT
-	 * check if an affiliate record has already been created.  That needs to be done by client code.
-	 *
-	 * @param EE_Transaction $transaction
-	 * @param Affiliate_WP   $awp
-	 */
-	protected static function _maybe_initiate_affiliate_tracking( EE_Transaction $transaction, Affiliate_WP $awp ) {
+    /**
+     * Callback for the AHEE__EEM_Transaction__delete_junk_transactions__successful_deletion action hook.
+     * Used to ensure that if there are any affiliate records matching the deleted transaction id that we delete those
+     * referrals.
+     *
+     * @param $deleted_transaction_ids
+     */
+    public static function set_affiliate_referral_status_after_deleted_transaction($deleted_transaction_ids)
+    {
+        if (is_array($deleted_transaction_ids)) {
+            $awp = function_exists('affiliate_wp') ? affiliate_wp() : null;
+            if ($awp instanceof Affiliate_WP
+            ) {
+                foreach ($deleted_transaction_ids as $txn_id) {
+                    if ($referral = $awp->referrals->get_by('reference', $txn_id, self::$_context)) {
+                        if (is_object($referral)
+                            && function_exists('affwp_delete_referral')
+                            && $referral->status !== 'paid'
+                        ) {
+                            affwp_delete_referral($referral);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-		//only do this if we have a valid affiliate id.
-		if ( ! $awp->tracking->is_valid_affiliate( $awp->tracking->get_affiliate_id() ) ) {
-			return;
-		}
 
-		//valid affiliate so let's get creating the initial affiliate record.
-		$invoice_amount = $transaction->total() > 0 ? affwp_calc_referral_amount( $transaction->total() ) : 0;
+    /**
+     * This will update a AffiliateWP referral record if it hasn't been paid according to the transaction status.
+     *
+     * @param EE_Transaction $transaction
+     * @param Affiliate_WP   $awp
+     * @param                $referral
+     */
+    protected static function _maybe_update_affiliate_referral_status(EE_Transaction $transaction, Affiliate_WP $awp, $referral)
+    {
+        // if $referral isn't an object or has already been paid, then get out
+        if (! is_object($referral)
+            || $referral->status === 'paid'
+            || ! function_exists('affwp_set_referral_status')
+        ) {
+            return;
+        }
 
-		//get events on transaction so we can setup the description for this purchase.
-		$registrations = $transaction->registrations();
-		$event_titles = array();
-		foreach ( $registrations as $registration ) {
-			if ( $registration instanceof EE_Registration ) {
-				if ( $registration->event_name() ) {
-					$event_titles[] = $registration->event_name();
-				}
-			}
-		}
+        // if transaction is complete, then let's update the status to unpaid and also ensure the total is correct for
+        // the current transaction. Otherwise we make sure the status is pending.
+        if ($transaction->is_completed()) {
+            // valid affiliate so let's get creating the initial affiliate record.
+            $invoice_amount = $transaction->total() > 0 ? affwp_calc_referral_amount($transaction->total(), $referral->affiliate_id) : 0;
+            affwp_set_referral_status($referral->referral_id, 'unpaid');
 
-		if ( $event_titles ) {
-			$description = count( $event_titles ) > 1 ? implode( ', ', $event_titles ) : $event_titles[0];
-		} else {
-			$description = '';
-		}
+            if ($invoice_amount !== $referral->amount) {
+                $awp->referrals->update($referral->referral_id, array( 'amount' => $invoice_amount ));
+            }
+        } else {
+            affwp_set_referral_status($referral->referral_id, 'pending');
+        }
+    }
 
-		//store visit in db
-		$referral_id = $awp->referrals->add(
-			array(
-				'affiliate_id' => $awp->tracking->get_affiliate_id(),
-				'amount' => $invoice_amount,
-				'status' => 'pending',
-				'description' => apply_filters( 'AHEE__EED_Affiliate_WP___maybe_initiate_affiliate_tracking__description', $description, $transaction, $event_titles ),
-				'context' => self::$_context,
-				'campaign' => $awp->tracking->get_campaign(),
-				'reference' => $transaction->ID(),
-				'visit_id' => $awp->tracking->get_visit_id()
-			)
-		);
 
-		//reset status if transaction is completed because AffiliateWP seems to have this as the canonical method for changing
-		//referral status (with actions etc on this method).
-		if (
-			$transaction->is_completed()
-			&& function_exists( 'affwp_set_referral_status' )
-		) {
-			affwp_set_referral_status( $referral_id, 'unpaid' );
-		}
 
-		$awp->visits->update( $awp->tracking->get_visit_id(), array( 'referral_id' => $referral_id ), '', 'visit' );
-	}
 
+    /**
+     * This will create an affiliate wp referral record if there is an affiliate ID in the request.  Note, this does NOT
+     * check if an affiliate record has already been created.  That needs to be done by client code.
+     *
+     * @param EE_Transaction $transaction
+     * @param Affiliate_WP   $awp
+     */
+    protected static function _maybe_initiate_affiliate_tracking(EE_Transaction $transaction, Affiliate_WP $awp)
+    {
+
+        // only do this if we have a valid affiliate id.
+        if (! $awp->tracking->is_valid_affiliate($awp->tracking->get_affiliate_id())) {
+            return;
+        }
+
+        // valid affiliate so let's get creating the initial affiliate record.
+        $invoice_amount = $transaction->total() > 0 ? affwp_calc_referral_amount($transaction->total()) : 0;
+
+        // get events on transaction so we can setup the description for this purchase.
+        $registrations = $transaction->registrations();
+        $event_titles = array();
+        foreach ($registrations as $registration) {
+            if ($registration instanceof EE_Registration) {
+                if ($registration->event_name()) {
+                    $event_titles[] = $registration->event_name();
+                }
+            }
+        }
+
+        if ($event_titles) {
+            $description = count($event_titles) > 1 ? implode(', ', $event_titles) : $event_titles[0];
+        } else {
+            $description = '';
+        }
+
+        // store visit in db
+        $referral_id = $awp->referrals->add(
+            array(
+                'affiliate_id' => $awp->tracking->get_affiliate_id(),
+                'amount' => $invoice_amount,
+                'status' => 'pending',
+                'description' => apply_filters('AHEE__EED_Affiliate_WP___maybe_initiate_affiliate_tracking__description', $description, $transaction, $event_titles),
+                'context' => self::$_context,
+                'campaign' => $awp->tracking->get_campaign(),
+                'reference' => $transaction->ID(),
+                'visit_id' => $awp->tracking->get_visit_id()
+            )
+        );
+
+        // reset status if transaction is completed because AffiliateWP seems to have this as the canonical method for changing
+        // referral status (with actions etc on this method).
+        if ($transaction->is_completed()
+            && function_exists('affwp_set_referral_status')
+        ) {
+            affwp_set_referral_status($referral_id, 'unpaid');
+        }
+
+        $awp->visits->update($awp->tracking->get_visit_id(), array( 'referral_id' => $referral_id ), '', 'visit');
+    }
 }
--- EE_Affiliate_Addon.class.php
+++ EE_Affiliate_Addon.class.php
@@ -1,9 +1,9 @@
-<?php if ( ! defined( 'EVENT_ESPRESSO_VERSION' )) { exit(); }
+<?php
 
 // define the plugin directory path and URL
-define( 'EE_AFFILIATE_WP_BASENAME', plugin_basename( EE_AFFILIATE_WP_PLUGIN_FILE ) );
-define( 'EE_AFFILIATE_WP_PATH', plugin_dir_path( __FILE__ ) );
-define( 'EE_AFFILIATE_WP_URL', plugin_dir_url( __FILE__ ) );
+define('EE_AFFILIATE_WP_BASENAME', plugin_basename(EE_AFFILIATE_WP_PLUGIN_FILE));
+define('EE_AFFILIATE_WP_PATH', plugin_dir_path(__FILE__));
+define('EE_AFFILIATE_WP_URL', plugin_dir_url(__FILE__));
 
 
 
@@ -11,37 +11,36 @@ define( 'EE_AFFILIATE_WP_URL', plugin_dir_url( __FILE__ ) );
  *
  * Class  EE_Affiliate_Addon
  *
- * @package			Event Espresso
- * @subpackage		eea-new-addon
- * @author			Darren Ethier
+ * @package         Event Espresso
+ * @subpackage      eea-new-addon
+ * @author          Darren Ethier
  *
  */
-Class  EE_Affiliate_Addon extends EE_Addon {
+class EE_Affiliate_Addon extends EE_Addon
+{
 
-	public static function register_addon() {
-		// register addon via Plugin API
-		EE_Register_Addon::register(
-			'EE_Affiliate_Addon',
-			array(
-				'version' 					=> EE_AFFILIATE_WP_VERSION,
-				'plugin_slug' 			=> 'eea-affiliatewp',
-				'min_core_version' => EE_AFFILIATE_WP_CORE_VERSION_REQUIRED,
-				'main_file_path' 		=> EE_AFFILIATE_WP_PLUGIN_FILE,
-				'autoloader_paths' => array(
-					'EE_Affiliate_Addon' 						=> EE_AFFILIATE_WP_PATH . 'EE_Affiliate_Addon.class.php',
-				),
-				'module_paths' => array( EE_AFFILIATE_WP_PATH . 'EED_Affiliate_WP.module.php' ),
-				// if plugin update engine is being used for auto-updates. not needed if PUE is not being used.
-				'pue_options'			=> array(
-					'pue_plugin_slug' 		=> 'eea-affiliatewp',
-					'plugin_basename' 	=> EE_AFFILIATE_WP_BASENAME,
-					'checkPeriod' 				=> '24',
-					'use_wp_update' 		=> false,
-				)
-			)
-		);
-
-	}
+    public static function register_addon()
+    {
+        // register addon via Plugin API
+        EE_Register_Addon::register(
+            'EE_Affiliate_Addon',
+            array(
+                'version'                   => EE_AFFILIATE_WP_VERSION,
+                'plugin_slug'           => 'eea-affiliatewp',
+                'min_core_version' => EE_AFFILIATE_WP_CORE_VERSION_REQUIRED,
+                'main_file_path'        => EE_AFFILIATE_WP_PLUGIN_FILE,
+                'autoloader_paths' => array(
+                    'EE_Affiliate_Addon'                        => EE_AFFILIATE_WP_PATH . 'EE_Affiliate_Addon.class.php',
+                ),
+                'module_paths' => array( EE_AFFILIATE_WP_PATH . 'EED_Affiliate_WP.module.php' ),
+                // if plugin update engine is being used for auto-updates. not needed if PUE is not being used.
+                'pue_options'           => array(
+                    'pue_plugin_slug'       => 'eea-affiliatewp',
+                    'plugin_basename'   => EE_AFFILIATE_WP_BASENAME,
+                    'checkPeriod'               => '24',
+                    'use_wp_update'         => false,
+                )
+            )
+        );
+    }
 }
-// End of file EE_AffiliateWP_Addon.class.phplass.php
-// Location: wp-content/plugins/eea-new-addon/EE_AffiliateWP_Addon.class.phplass.php
